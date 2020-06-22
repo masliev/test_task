14757038a6014cce953556dc74dd3ec88dcb9e16
Ilya Masliev <ilamasliev@Ilyas-MacBook-Air.local>
2020-06-22 15:11:57 +0300

    changed get declaration of dict

--- template_generator.py
+++ template_generator.py
@@ -1,18 +1,19 @@
-
 template_required_fields = ('platform_name', 'body_text')
+
 
 def template_generator(*args, **kwargs):
     for i in template_required_fields:
-        if kwargs[i]:
-            pass
-        else:
-            raise Exception('Required fields are empty, required fields is {}'.format(template_required_fields))
+        try: 
+            kwargs[i]
+        except KeyError:
+            raise Exception('Required fields are empty, required fields is {}'\
+                            .format(template_required_fields))
     configuration_template = {
-        "platform_name":"{}".format(kwargs[platform_name]),
-        "site_title": "{}".format(kwargs[platform_name]),
-        "body_text": "{}".format(kwargs[body_text]),
+        "platform_name":"{}".format(kwargs['platform_name']),
+        "site_title": "{}".format(kwargs['platform_name']),
+        "body_text": "{}".format(kwargs['body_text']),
         "footer": {
-            "footer_header": "Copyright by {}".format(kwargs[platform_name]),
+            "footer_header": "Copyright by {}".format(kwargs['platform_name']),
         },
     }
     return configuration_template
