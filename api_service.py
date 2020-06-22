14955d042d00cd7522a013271e44e27d9663443c
Ilya Masliev <ilamasliev@Ilyas-MacBook-Air.local>
2020-06-22 15:17:13 +0300

    request parsing with questionnaire id selecting

--- api_service.py
+++ api_service.py
@@ -7,12 +7,14 @@ QUESTIONS_MAP = {
     "lUSCoWCfRBjz": "platform_name",
     "xzEOKIHDo4SA": "body_text",
 }
+QUESTIONNAIRE_ID = 1
 
 class APIService:
-    def __init__(self, url, token, questions_map):
+    def __init__(self, url, token, questions_map, questionnaire_id):
         self.url = url
         self.token = token
         self.questions_map = questions_map
+        self.questionnaire_id = questionnaire_id
 
     @lru_cache(maxsize=128, typed=False)
     def _api_get_results(self, url, token):
@@ -21,7 +23,9 @@ class APIService:
         return response.json()
     
     def prepared_results(self):
-        answers = self._api_get_results(self.url, self.token)['items'][0]['answers']
+        q_id = self.questionnaire_id
+        answers = self._api_get_results(self.url, self.token)\
+                                        ['items'][q_id]['answers']
         key_value_arguments = {}
         for answer in answers:
             answer_id = answer['field']['id']
@@ -30,7 +34,7 @@ class APIService:
         return key_value_arguments
 
 
-def apiservice_factory(url, token, questions_map):
-    return APIService(url, token, questions_map)
+def apiservice_factory(url, token, questions_map, questionnaire_id):
+    return APIService(url, token, questions_map, questionnaire_id)
 
-api_service = apiservice_factory(URL, TOKEN, QUESTIONS_MAP)
+api_service = apiservice_factory(URL, TOKEN, QUESTIONS_MAP, QUESTIONNAIRE_ID)
