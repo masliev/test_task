a287367d338fab2852969e3bcc18caea2d8e4ecd
Ilya Masliev <ilamasliev@Ilyas-MacBook-Air.local>
2020-06-22 11:47:58 +0300

    configuration_template declaration

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ mocked_response = json.dumps({
     "platform_name": "AwesomeMarketplace",
     "body_text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
 })
+
+configuration_template = {
+    "platform_name":"",
+    "site_title": "",
+    "body_text": "",
+    "footer": {
+        "footer_header": "",
+        "footer_body": "",
+    },
+}
