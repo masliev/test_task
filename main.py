60604cea42bf782696c4539295c828b75f604f6b
Ilya Masliev <ilamasliev@Ilyas-MacBook-Air.local>
2020-06-22 15:12:51 +0300

    clean main script

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import json
-
-from template_generator import template_generator
-from config_service import ConfigService
-
-
-mocked_response = json.dumps({
-    "platform_name": "AwesomeMarketplace",
-    "body_text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-})
-
+from config_service import service
 
 if __name__ == "__main__":
-    pass
+    print(service.create_config())
